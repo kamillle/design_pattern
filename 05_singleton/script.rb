4350b808150e9bf5eabd095bdceb5daa7ca2f062
kamillle <yuji.kmjm@gmail.com>
2019-07-17 02:37:53 +0900

    singleton pattern

---
+++ 05_singleton/script.rb
@@ -0,0 +1,40 @@
+class Singleton
+  class << self
+    def instance
+      singleton
+    end
+
+    private
+
+    # メモ化しておくことで何度初期化しても同じobject_idのインスタンスが返るようにする
+    def singleton
+      @singleton ||= self.new
+    end
+  end
+
+  private
+
+  # constメソッドはprivate化して、外から .new が実行できないようにする
+  def initialize
+    puts 'インスタンスを生成しました'
+  end
+end
+
+class Main
+  def self.execute(args = nil)
+    puts 'start'
+
+    singleton1 = Singleton.instance
+    singleton2 = Singleton.instance
+
+    if singleton1.object_id == singleton2.object_id
+      puts 'singleton1 is same object as singleton2'
+    else
+      puts 'singleton1 is not same object as singleton2'
+    end
+
+    puts 'end'
+  end
+end
+
+Main.execute
